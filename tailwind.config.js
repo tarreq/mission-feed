2bc2b7ecf0e3478fe9d765c15dc00582592d28e5
Tareq Hasan <tarreeq@gmail.com>
2021-11-20 04:45:14 +0200

    Add and configure Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontSize: {
+        'xxs': '0.35rem',
+    }
+    },
+    fontFamily: {
+      'roboto': ['Roboto'],
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
